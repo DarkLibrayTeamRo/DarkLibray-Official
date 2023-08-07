289287643f80124d3b5bb53a0fdf3fcbfb0e5dd5
DarkLibrayTeamRo <141654158+DarkLibrayTeamRo@users.noreply.github.com>
2023-08-07 16:17:49 +0100

    Update .lua

--- .lua
+++ .lua
@@ -43,4 +43,21 @@ function DarkLibrary.CreateToggle(name, text, parent, position, size)
     return toggle, event.Event
 end
 
+function DarkLibrary:Load(windowName, windowStyle)
+    local window = Instance.new("Frame")
+    window.Size = UDim2.new(0, 200, 0, 150)
+    window.Style = windowStyle or Enum.FrameStyle.RobloxRound
+    window.Name = windowName
+    window.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+    local title = Instance.new("TextLabel")
+    title.Text = windowName
+    title.Size = UDim2.new(1, 0, 0, 30) -- Full width, 30 pixels high
+    title.BackgroundTransparency = 1
+    title.TextColor3 = Color3.new(1, 1, 1) -- White
+    title.Parent = window
+
+    return window
+end
+
 return DarkLibrary
