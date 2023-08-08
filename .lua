40a64c591dbf4d3211a0fdef630db44737fb470a
DarkLibrayTeamRo <141654158+DarkLibrayTeamRo@users.noreply.github.com>
2023-08-08 20:53:20 +0100

    Update .lua

--- .lua
+++ .lua
@@ -1,63 +1,73 @@
-local DarkLibrary = {}
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "UltraMods", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Player Menu",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "Speed",
+	Callback = function()
+	local WalkSpeed = 98
+ 
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 98
+      		print("button pressed")
+  	end
+})
+Tab:AddButton({
+	Name = "Infinity Jump",
+	Callback = function()
+	local infjmp = true
+game:GetService("UserInputService").jumpRequest:Connect(function()
+    if infjmp then
+        game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass"Humanoid":ChangeState("Jumping")
+    end
+end)
+      		print("button pressed")
+  	end
+})
+  Tab:AddButton({
+	Name = "Esp",
+	Callback = function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Survive%20the%20Killer.txt'))()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "NoClip",
+	Callback = function()
+	local Noclip = nil
+local Clip = nil
 
-function DarkLibrary.CreateButton(name, text, parent, position, size)
-    local button = Instance.new("TextButton")
-    button.Name = name
-    button.Text = text
-    button.Parent = parent
-    button.Position = position or UDim2.new(0.5, 0, 0.5, 0)
-    button.Size = size or UDim2.new(0, 200, 0, 50)
-    button.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
-    return button
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
 end
 
-function DarkLibrary.CreateLabel(name, text, parent, position, size)
-    local label = Instance.new("TextLabel")
-    label.Name = name
-    label.Text = text
-    label.Parent = parent
-    label.Position = position or UDim2.new(0.5, 0, 0.5, 0)
-    label.Size = size or UDim2.new(0, 200, 0, 50)
-    label.BackgroundTransparency = 1
-    label.TextColor3 = Color3.new(1, 1, 1)
-    return label
+function clip()
+	if Noclip then Noclip:Disconnect() end
+	Clip = true
 end
 
-function DarkLibrary.CreateToggle(name, text, parent, position, size)
-    local toggle = Instance.new("TextButton")
-    toggle.Name = name
-    toggle.Text = text
-    toggle.Parent = parent
-    toggle.Position = position or UDim2.new(0.5, 0, 0.5, 0)
-    toggle.Size = size or UDim2.new(0, 200, 0, 50)
-    toggle.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
-
-    local active = false
-    local event = Instance.new("BindableEvent")
-    
-    toggle.MouseButton1Click:Connect(function()
-        active = not active
-        event:Fire(active)
-    end)
-
-    return toggle, event.Event
-end
-
-function DarkLibrary:Load(windowName, windowStyle)
-    local window = Instance.new("Frame")
-    window.Size = UDim2.new(0, 200, 0, 150)
-    window.Style = windowStyle or Enum.FrameStyle.RobloxRound
-    window.Name = windowName
-    window.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-    local title = Instance.new("TextLabel")
-    title.Text = windowName
-    title.Size = UDim2.new(1, 0, 0, 30) -- Full width, 30 pixels high
-    title.BackgroundTransparency = 1
-    title.TextColor3 = Color3.new(1, 1, 1) -- White
-    title.Parent = window
-
-    return window
-end
-
-return DarkLibrary
+noclip() -- to toggle noclip() and clip()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "Reset Character",
+	Callback = function()
+	game.Players.LocalPlayer.Character.Head:Destroy()
+      		print("button pressed")
+  	end    
+})
+OrionLib:Init()
