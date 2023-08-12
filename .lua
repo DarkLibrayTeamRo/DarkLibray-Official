3678a5a2bf38007ae3e462bb0f04e371d44b3bce
DarkLibrayTeamRo <141654158+DarkLibrayTeamRo@users.noreply.github.com>
2023-08-12 20:03:13 +0100

    Update .lua

--- .lua
+++ .lua
@@ -70,3 +70,24 @@ end
 		end
     print("Clicked")
 end)
+Section:NewButton("Autu Kill all ServSide Only!", "its not kill everyone you will teleport to player automatic then yoyr need to kill but its cool its only work on ServerSide", function()
+		spawn(function()
+    while wait(0.00000000000000000000000000000000000000000000000000000000000000000001) do
+        for i, v in next, game:GetService("Players"):GetChildren() do
+            pcall(function()
+                if v~= game:GetService("Players").LocalPlayer and not v.Character:FindFirstChildOfClass("ForceField") and v.Character.Humanoid.Health > 0 then
+                    while v.Character:WaitForChild("Humanoid").Health > 0 do
+                        wait();
+                        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame;
+                        for x, c in next, game:GetService("Players"):GetChildren() do
+                            if c ~= game:GetService("Players").LocalPlayer then game.ReplicatedStorage.meleeEvent:FireServer(c) end
+                        end
+                    end
+                end
+            end)
+            wait()
+        end
+   end
+end)
+    print("Clicked")
+end)
